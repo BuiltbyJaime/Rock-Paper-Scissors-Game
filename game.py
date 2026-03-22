2bed499512452ad2361bda3c5691fe2279ccc9cc
Jaime <jai01me@me.com>
2026-03-21 22:50:03 -0400

    Added game logic as well as the option for the user to play again

--- game.py
+++ game.py
@@ -5,20 +5,64 @@ options = ["rock", "paper", "scissors"]
 
 input("Dare to play? ")
 
-time.sleep(2)
-
-print("Haha, You have no control here!!")
-
-time.sleep(2)
+time.sleep(3)
 
 
-player_choice = str(input("What is your choice?: ")).lower()
+#Included the code in a function so I could call it again if the player chooses to 
 
-if player_choice in options:
-    print(f'You have chosen {player_choice}, good luck!')
-else:
-    print(f'{player_choice} is not a valid entry, try again !')
-  
+ # Added the ability for the player to choose to play again or not after initial choice 
+
+def choosingAgain ():
+        again = input ("Want to play again? (yes/no) ")
+        if again.lower()== "yes":
+            gamelogic()
+        if again.lower()== "no":
+            print ("Have a good day!")
+            exit
+        else:
+            print("Please type Yes or No")
+            choosingAgain()
 
 
-computerChoice = random.choice(options)
+def gamelogic():
+    
+    player_choice = str(input("What is your choice?: ")).lower()    
+    if player_choice in options:
+        print(f'You have chosen {player_choice}, good luck!')
+        time.sleep(1)
+    else: 
+        print(f'{player_choice} is not a valid entry, try again !')
+        gamelogic()
+    
+    time.sleep(1)
+    print("Computer is thinking about choosing")
+    time.sleep(2)  
+
+    
+    
+    # Here is the game logic for the choices including a tie 
+    computerChoice = random.choice(options)
+    if player_choice == computerChoice:
+        print("It is a tie !")
+        choosingAgain()
+    elif player_choice == "rock" and computerChoice == "paper":
+        print (f"You have lost, Computer chose {computerChoice}")
+        choosingAgain()
+    elif player_choice == "rock" and computerChoice == "scissors":
+        print(f"Congratulations you have won! Computer chose {computerChoice} ")
+        choosingAgain()
+    elif player_choice == "paper" and computerChoice == "rock":
+        print (f"Congratulations you have won! Computer chose {computerChoice}")
+        choosingAgain()
+    elif player_choice == "paper" and computerChoice == "scissors":
+        print(f"You have lost, Computer chose {computerChoice}")
+        choosingAgain()
+    elif player_choice == "scissors" and computerChoice == "paper":
+        print (f"Congratulations you have won! Computer chose {computerChoice}")
+        choosingAgain()
+    elif player_choice == "scissors" and computerChoice == "rock":
+        print(f"You have lost, Computer chose {computerChoice}")
+        choosingAgain()
+
+gamelogic()
+
